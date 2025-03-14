39430cfd04c56bda58f8f2eb64c4395cfb7aa92e
guphilip <guillaumephilippe.digital@gmail.com>
2025-03-14 18:39:36 +0100

    + | npm installs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
+
+console.log(0)
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <RouterView />
 </template>
 
 <style scoped>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<h1>Home</h1>
+		<div class="box" ref="box">box</div>
+	</div>
+</template>
+
+<script setup>
+import { onBeforeRouteLeave } from 'vue-router';
+import {onMounted} from 'vue';
+import gsap from 'gsap';
+import { ref } from 'vue';
+
+const box = ref(null)
+
+onMounted(() => {
+	console.log(box.value);
+
+	gsap.from(box.value,
+	{ rotation: 27,
+		x: 100,
+		duration: 1,
+		onComplete: () => {
+		}
+	});
+}),
+
+onBeforeRouteLeave((to, from, next) => {
+	console.log('frerot je pars');
+
+	gsap.to(box.value,
+	{ rotation: 27,
+		y: 100,
+		duration: 1,
+		onComplete: () => {
+			next();
+		}
+	});
+})
+</script>
+
+<style lang="scss" scoped>
+	.box {
+		background-color: red;
+	}
+</style>
